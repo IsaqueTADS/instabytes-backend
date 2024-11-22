1fd746b3bc7b49096900cf4d2f0e5cd4a15d07bd
IsaqueTADS <isaqueofc18@gmail.com>
2024-11-22 12:05:14 -0300

    aula 4 finalizada

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,4 +1,5 @@
 import { criarPost, getTodosPosts } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarposts(req, res) {
   const posts = await getTodosPosts();
@@ -12,6 +13,23 @@ export async function postarNovoPost(req, res) {
     res.status(200).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
-    res.status(500).json({ "Erro": "Falha na requisição" });
+    res.status(500).json({ Erro: "Falha na requisição" });
+  }
+}
+
+export async function uploadImagem(req, res) {
+  const novoPost = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+  try {
+    const postCriado = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ erro: "falha na requisição" });
   }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { listarposts, postarNovoPost } from "../controller/postsController.js";
+import multer from "multer";
+import { listarposts, postarNovoPost, uploadImagem } from "../controller/postsController.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
   app.get("/posts", listarposts);
-  app.post("/posts", postarNovoPost)
+  app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;

09a50d53ce6ce973e0d4ff8b33e89cac15383ee2
IsaqueTADS <isaqueofc18@gmail.com>
2024-11-22 09:23:25 -0300

    metade aula 3

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const post = [
   {
     id: 1,
     descricao: "Foto teste",
-    imagem: "https://playcecats.com/millie/300/150",
+    imagem: "https://placecats.com/millie/300/150",
   },
   {
     id: 2,
     descricao: "Foto teste",
-    imagem: "https://playcecats.com/millie/300/150",
+    imagem: "https://placecats.com/millie/300/150",
   },
   {
     id: 3,
@@ -46,16 +48,22 @@ app.listen(3000, () => {
   console.log("ligou bb...");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(post);
+async function getTodosPosts() {
+    const db = conexao.db("instabytes")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+}
+app.get("/posts", async (req, res) => {
+  const posts = await getTodosPosts()
+  res.status(200).json(posts);
 });
 
-function buscarPostID(id) {
-  return post.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostID(req.params.id);
-  res.status(200).json(post[index]);
-});
+// function buscarPostID(id) {
+//   return post.findIndex((post) => {
+//     return post.id === Number(id);
+//   });
+// }
+// app.get("/posts/:id", (req, res) => {
+//   const index = buscarPostID(req.params.id);
+//   res.status(200).json(post[index]);
+// });

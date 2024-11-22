4f4f870ce6c8e0108c640b914a61044783c38ff8
IsaqueTADS <isaqueofc18@gmail.com>
2024-11-22 01:24:57 -0300

    segunda aula concluida com sucesso

--- server.js
+++ server.js
@@ -2,30 +2,37 @@ import express from "express";
 
 const post = [
   {
-    decricao: "Foto teste",
-    imagem: "https://playcecats.com/millie/300/150",
-  },
-  {
+    id: 1,
     descricao: "Foto teste",
     imagem: "https://playcecats.com/millie/300/150",
   },
   {
+    id: 2,
+    descricao: "Foto teste",
+    imagem: "https://playcecats.com/millie/300/150",
+  },
+  {
+    id: 3,
     descricao: "Paisagem montanhosa",
     imagem: "https://placekitten.com/300/150",
   },
   {
+    id: 4,
     descricao: "Pôr do sol na praia",
     imagem: "https://placekitten.com/310/160",
   },
   {
+    id: 5,
     descricao: "Flores no jardim",
     imagem: "https://placekitten.com/320/170",
   },
   {
+    id: 6,
     descricao: "Cidade à noite",
     imagem: "https://placekitten.com/330/180",
   },
   {
+    id: 7,
     descricao: "Cachoeira tranquila",
     imagem: "https://placekitten.com/340/190",
   },
@@ -39,10 +46,16 @@ app.listen(3000, () => {
   console.log("ligou bb...");
 });
 
-app.get("/post", (req, res) => {
-  res
-    .status(200)
-    .json(
-      post
-    );
+app.get("/posts", (req, res) => {
+  res.status(200).json(post);
+});
+
+function buscarPostID(id) {
+  return post.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostID(req.params.id);
+  res.status(200).json(post[index]);
 });

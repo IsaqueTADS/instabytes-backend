5e5b876e0af75f36432f4dfd19282df7be76f532
IsaqueTADS <isaqueofc18@gmail.com>
2025-05-10 00:25:46 -0300

    rota de teste

--- Instabyte-backend/server.js
+++ Instabyte-backend/server.js
@@ -3,6 +3,9 @@ import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
 app.use(express.static("uploads"));
+qpp.get("/", (req, res) => {
+  return res.send("Funcionou");
+});
 routes(app);
 
 const PORT = process.env.PORT || 3000;
